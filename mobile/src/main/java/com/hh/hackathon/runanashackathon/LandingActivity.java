5c84c374d184c57cb06d23b1643719894df9464d
Maja Jaensch <maja.jaensch@freiheit.com>
2015-05-23 19:31:17 +0200

    Initialstand

---
+++ mobile/src/main/java/com/hh/hackathon/runanashackathon/LandingActivity.java
@@ -0,0 +1,48 @@
+package com.hh.hackathon.runanashackathon;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class LandingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+        findViewById(R.id.leaveLandingPage).setOnClickListener(sendNotificationClickListener);
+    }
+
+    View.OnClickListener sendNotificationClickListener = new View.OnClickListener(){
+        public void onClick(View view) {
+            Intent startLogin = new Intent(LandingActivity.this, LoginActivity.class);
+            startActivity(startLogin);
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_landing, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
